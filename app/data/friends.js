8723b2344713f20fb182f42c6018728aeb15db7f
Kevin Griffith <kevingriffith84@gmail.com>
2018-04-28 08:47:36 -0700

    hwk incomplete

---
+++ app/data/friends.js
@@ -0,0 +1,101 @@
+// Data structure that will store the friends data
+var friendsArray = [
+	{
+		name: 'Joey',
+		photo: 'http://www.boxpop.com.br/as-10-melhores-frases-do-inesquecivel-joey-tribbiani/',
+		scores: [
+			'1',
+			'5',
+			'5',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Rachel',
+		photo: 'https://www.hindustantimes.com/tv/happy-birthday-jennifer-aniston-take-this-dialogues-quiz-to-prove-you-are-a-true-rachel-green-fan/story-PpCldQdGdbAkAFUpAcfBQO.html',
+		scores: [
+			'2',
+			'4',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'2',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Ross',
+		photo: 'http://es.friends.wikia.com/wiki/Ross_Geller',
+		scores: [
+			'4',
+			'3',
+			'5',
+			'1',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'Monica',
+		photo: 'http://friends.wikia.com/wiki/Monica_Geller-Bing',
+		scores: [
+			'5',
+			'1',
+			'2',
+			'1',
+			'3',
+			'4',
+			'1',
+			'3',
+			'3',
+			'4'
+		]
+	},
+	{
+		name: 'Phoebe',
+		photo: 'https://hellogiggles.com/reviews-coverage/tv-shows/friends-halloween-costumes/',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'1'
+		]
+	},
+	{
+		name: 'Chandler',
+		photo: 'http://friends.wikia.com/wiki/Chandler_Bing',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'1',
+			'3',
+			'2',
+			'5',
+			'1',
+			'1',
+			'5'
+		]
+	},
+];
+
+module.exports = friendsArray;
